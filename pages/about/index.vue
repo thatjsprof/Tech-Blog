0e5a593517bacc0c56b96d3eddd635927f435455
David Ajayi <davidrums65@gmail.com>
2021-02-24 15:42:20 +0100

    added about page files

---
+++ pages/about/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="about-page">
+        <Header />
+        <main>
+            <div class="row text-center">
+                <h2 class="u-margin-bottom-big">About Us</h2>
+                <p>This blog is focused on tech news haha</p>
+            </div>
+        </main>
+    </div>
+</template>
+
+<script>
+    import Header from '@/components/Header/header.vue'
+
+    export default {
+        components: {
+            Header
+        },
+        data () {
+            return {
+                
+            }
+        }
+    }
+</script>
